0268f141e7ad910fa6ac41c4a7b27729650cc977
Cris <cristian.correa.cs@gmail.com>
2025-09-11 01:44:37 -0500

    Committing changes for app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
-import { ThemeProvider } from "@/context/ThemeContext"
+import { ThemeProvider } from "@/context/theme-context"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
